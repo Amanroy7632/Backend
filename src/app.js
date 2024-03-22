fc18ae49371f9ef0baabc6cc1f77074589b26067
Aman Roy <yadavaman7632@gmail.com>
2024-03-23 00:37:59 +0530

    added utils

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
 import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser';
 const app= express();
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+app.use(express.json({limit:"16kb"})) //use methods are used to configurations
+app.use(express.urlencoded({extended:true,limit:"16kb"})) //use are also known as middleware
+app.use(express.static("public")) //It creates a public assets directory
+app.use(cookieParser())
 export {app};
